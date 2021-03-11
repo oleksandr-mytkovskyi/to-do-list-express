8b73f4fb471b7d4b539b183109713bf61d04c30f
Alex Mytkovskyi <askona1995@gmail.com>
2021-03-11 14:29:52 +0200

    test production db2

--- app/models/index.js
+++ app/models/index.js
@@ -1,21 +1,9 @@
 const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 
-// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-//   host: dbConfig.HOST,
-//   dialect: dbConfig.dialect,
-//   operatorsAliases: false,
-//   // dialectOptions: {
-//   //   ssl: {
-//   //     require: true, // This will help you. But you will see nwe error
-//   //     rejectUnauthorized: false // This line will fix new error
-//   //   }
-//   // },
-// });
-
-const sequelize = new Sequelize(ENV['DB'], ENV['USER'], ENV['PASSWORD'], {
-  host: ENV['HOST'],
-  dialect: ENV['dialect'],
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
   operatorsAliases: false,
   dialectOptions: {
     ssl: {
@@ -24,6 +12,18 @@ const sequelize = new Sequelize(ENV['DB'], ENV['USER'], ENV['PASSWORD'], {
     }
   },
 });
+
+// const sequelize = new Sequelize(ENV['DB'], ENV['USER'], ENV['PASSWORD'], {
+//   host: ENV['HOST'],
+//   dialect: ENV['dialect'],
+//   operatorsAliases: false,
+//   dialectOptions: {
+//     ssl: {
+//       require: true, // This will help you. But you will see nwe error
+//       rejectUnauthorized: false // This line will fix new error
+//     }
+//   },
+// });
 
 const db = {};
 
