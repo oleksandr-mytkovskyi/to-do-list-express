bf2584917400518f6deba78bcd740ac560fc5444
Alex Mytkovskyi <askona1995@gmail.com>
2021-03-18 09:51:49 +0200

    fix bug with access middleware

--- app/middleware/access.middleware.js
+++ app/middleware/access.middleware.js
@@ -1,11 +1,15 @@
+const e = require("express");
+
 exports.accessMiddleWare = async (req, res, next) => {
     try {
         if(req.roleId === 3 && req.method === 'GET') {
             next();
         } else if(req.roleId === 3){
             throw new Error('you does not have permision');
-        } else {
+        } else if(req.roleId === 1 || req.roleId === 2){
             next();
+        } else {
+            throw new Error('you does not have permision');
         }
     } catch(e) {
         res.status(401).send({
--- app/middleware/jwt.middleware.js
+++ app/middleware/jwt.middleware.js
@@ -2,10 +2,11 @@ const jwt = require('../modules/jwt');
 
 exports.jwtMiddleWare = async (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
-        if(!token) {
+        const authData = req.headers.authorization;
+        if(!authData) {
             throw new Error('Does not token, you need authorization')
         }
+        const token = authData.split(' ')[1];
         const parseToken =  jwt.checkToken(token, {type: "access"});
         if(!parseToken) {
             throw new Error('Token not valid');
